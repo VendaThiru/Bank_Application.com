304fe150f92f1b55f893a3f7a4630f0745da8540
M <vendamottaiyan@gmail.com>
2025-01-23 22:01:11 +0530

    commit
    commit

---
+++ Bank.java
@@ -0,0 +1,7 @@
+public class Bank {
+    public static void main(String[] args){
+        System.out.println("$$$ WELCOME TO Venda_Thiru BANK $$$");
+       new BankUI();
+        System.out.println("THANK YOU FOR VISITING...");
+    }
+}
